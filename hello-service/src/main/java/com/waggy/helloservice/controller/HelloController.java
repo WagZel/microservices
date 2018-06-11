c272e1bca3931fb2834209097442134cc45e6ae4
Ivan Zelinov <wag6051@gmail.com>
2018-06-12 03:15:57 +0500

    fix

--- hello-service/src/main/java/com/waggy/helloservice/controller/HelloController.java
+++ hello-service/src/main/java/com/waggy/helloservice/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.waggy.helloservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
